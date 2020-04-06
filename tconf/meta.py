fd5422d1ccee4c7041cb1e793305df985506ed19
Gringo Suave <github@mgmiller.net>
2020-04-05 19:17:06 -0700

    bump_vers

--- tconf/meta.py
+++ tconf/meta.py
@@ -8,7 +8,7 @@ from time import localtime as _localtime
 
 
 pkgname         = 'tconf'
-__version__     = version = '0.59a1'
+__version__     = version = '0.59a2'
 __author__      = authors = ', '.join([
                                 'Mike Miller',
                                 #~ 'and contributors',
